383f5b233503d4e9ac2915d4752297ec6898038c
vaijenyavaiju1 <vaijenyavaiju123@gmail.com>
2024-02-29 06:01:52 +0000

    solution

---
+++ src/main/java/com/example/employee/repository/EmployeeRepository.java
@@ -0,0 +1,17 @@
+package com.example.employee.repository;
+
+import java.util.*;
+
+import com.example.employee.model.*;
+
+public interface EmployeeRepository {
+    ArrayList<Employee> getEmployees();
+
+    Employee getEmployeeById(int employeeId);
+
+    Employee addEmployee(Employee employee);
+
+    Employee updateEmployee(int employeeId, Employee employee);
+
+    void deleteEmployee(int employeeId);
+}
---
+++ src/main/java/com/example/employee/service/EmployeeH2Service.java
@@ -0,0 +1,72 @@
+package com.example.employee.service;
+
+import com.example.employee.model.EmployeeRowMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import java.util.ArrayList;
+import com.example.employee.model.Employee;
+import com.example.employee.repository.EmployeeRepository;
+import java.util.*;
+
+// Write your code here
+
+@Service
+public class EmployeeH2Service implements EmployeeRepository {
+    @Autowired
+    private JdbcTemplate db;
+
+    @Override
+    public ArrayList<Employee> getEmployees() {
+        List<Employee> employeeList = db.query("select * from employeelist", new EmployeeRowMapper());
+        ArrayList<Employee> employees = new ArrayList<>(employeeList);
+        return employees;
+    }
+
+    @Override
+    public Employee getEmployeeById(int employeeId) {
+        try {
+            Employee employee = db.queryForObject("select * from employeelist where employeeId = ?",
+                    new EmployeeRowMapper(),
+                    employeeId);
+            return employee;
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @Override
+    public Employee addEmployee(Employee employee) {
+        db.update("insert into employeelist(employeeName, email, department) values(?, ?, ?)",
+                employee.getEmployeeName(),
+                employee.getEmail(), employee.getDepartment());
+        Employee savedEmployee = db.queryForObject(
+                "select * from employeelist where employeeName=? and email=? and department=?",
+                new EmployeeRowMapper(), employee.getEmployeeName(),
+                employee.getEmail(), employee.getDepartment());
+        return savedEmployee;
+    }
+
+    @Override
+    public Employee updateEmployee(int employeeId, Employee employee) {
+        if (employee.getEmployeeName() != null) {
+            db.update("update employeelist Set employeeName=? where employeeId=?", employee.getEmployeeName(),
+                    employeeId);
+        }
+        if (employee.getEmail() != null) {
+            db.update("update employeelist Set email=? where employeeId=?", employee.getEmail(), employeeId);
+        }
+        if (employee.getDepartment() != null) {
+            db.update("update employeelist Set department=? where employeeId=?", employee.getDepartment(), employeeId);
+        }
+        return getEmployeeById(employeeId);
+    }
+
+    @Override
+    public void deleteEmployee(int employeeId) {
+        db.update("delete from employeelist where employeeId=?", employeeId);
+    }
+
+}
